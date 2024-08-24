7b871e4ac2789761e569db537b59d60577400e6b
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-08-24 21:05:20 +0500

    completed addresses backend

--- Task 03/backend/controllers/address.controller.js
+++ Task 03/backend/controllers/address.controller.js
@@ -1,9 +1,9 @@
-import Address from '../models/address.model.js'
+import Address from "../models/address.model.js";
 
 export const createAddress = async (req, res) => {
-  const { studentID, street, hometown } = req.body;
+  const { student_id, street, hometown } = req.body;
   try {
-    const address = new Address({ student_id: studentID, street, hometown });
+    const address = new Address({ student_id, street, hometown });
     await address.save();
     res.status(201).json(address);
   } catch (error) {
@@ -16,21 +16,48 @@ export const createAddress = async (req, res) => {
 export const getAddressesByID = async (req, res) => {
   const { id } = req.params;
   try {
-    const addresses = await Address.find({ _id: id });
+    const addresses = await Address.findOne({ _id: id });
     res.status(200).json(addresses);
   } catch (error) {
     res.status(500).json({ message: "Error retrieving addresses" });
   }
+  next();
 };
 
 export const getAddressesByStudentID = async (req, res) => {
-  const { studentID } = req.params;
+  const { id } = req.params;
   try {
-    const addresses = await Address.find({ student_id: studentID });
+    const addresses = await Address.find({ student_id: id });
     res.status(200).json(addresses);
   } catch (error) {
     res.status(500).json({ message: "Error retrieving addresses" });
   }
+  next();
 };
 
-export const updateAddress = async (req, res) => {};
+export const updateAddress = async (req, res) => {
+  const { id } = req.params;
+  const { street, hometown } = req.body;
+  try {
+    const updatedAddress = await Address.findOneAndUpdate(
+      { _id: id },
+      { street, hometown }
+    );
+    if (!updatedAddress)
+      return res.status(404).json({ message: "Address not found" });
+    await updatedAddress.save();
+    res.status(200).json(updatedAddress);
+  } catch (err) {
+    res.status(500).json({ message: "Error updating address" });
+  }
+};
+
+export const deleteAddress = async (req, res) => {
+  try {
+    const address = await Address.findByIdAndDelete(req.params.id);
+    if (!address) return res.status(404).json({ message: "Address not found" });
+    res.status(200).json({ message: "Address deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting address" });
+  }
+};
--- Task 03/backend/routes/address.routes.js
+++ Task 03/backend/routes/address.routes.js
@@ -1,14 +1,18 @@
 import express from "express";
 import {
   createAddress,
+  deleteAddress,
   getAddressesByID,
   getAddressesByStudentID,
+  updateAddress,
 } from "../controllers/address.controller.js";
 
 const router = express.Router();
 
 router.post("/create-address", createAddress);
-router.get("/get-address-by-student-id/:studentID", getAddressesByStudentID);
+router.get("/get-address-by-student-id/:id", getAddressesByStudentID);
 router.get("/get-address-by-id/:id", getAddressesByID);
+router.post("/update-address/:id", updateAddress);
+router.get("/delete-address/:id", deleteAddress);
 
 export default router;
