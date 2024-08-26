bf6139917f6bb58794d1a9f427960bd889da812a
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-08-26 11:38:12 +0500

    student update is working fine with and without image

--- Task 03/frontend/src/components/dialogs/StudentUpdateDialog.jsx
+++ Task 03/frontend/src/components/dialogs/StudentUpdateDialog.jsx
@@ -66,15 +66,15 @@ const StudentUpdateDialog = ({
       alert("Please fill all required fields");
       return;
     }
-    // const formData = new FormData();
+    const formData = new FormData();
 
     // // formDataWithoutImage.append("id", id);
     // // formDataWithoutImage.append("name", name);
     // // formDataWithoutImage.append("email", email);
-    // formData.append("id", id);
-    // formData.append("name", name);
-    // formData.append("email", email);
-    // formData.append("image", imageFile);
+    formData.append("id", id);
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("image", imageFile);
     const routeWithImage = "update-student";
     const routeWithoutImage = "update-student-no-image";
     // console.log(formDataWithoutImage);
@@ -84,12 +84,12 @@ const StudentUpdateDialog = ({
         name: name,
         email: email,
       };
-      const formData = {
-        id: id,
-        name: name,
-        email: email,
-        image: imageFile,
-      };
+      // const formData = {
+      //   id: id,
+      //   name: name,
+      //   email: email,
+      //   image: imageFile,
+      // };
       console.log(formData);
       await fetch(
         `http://localhost:3001/api/students/${
@@ -97,12 +97,12 @@ const StudentUpdateDialog = ({
         }/${_id}`,
         {
           method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: !imageFile
-            ? JSON.stringify(formDataWithoutImage)
-            : JSON.stringify(formData),
+          headers: imageFile
+            ? undefined
+            : {
+                "Content-Type": "application/json",
+              },
+          body: !imageFile ? JSON.stringify(formDataWithoutImage) : formData,
         }
       )
         .then(async (response) => {
