6779041ab3319295af0e15d3379a1e8dda002883
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-08-28 21:01:11 +0500

    initiated task about theme

---
+++ Task 05/backend/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://hammadaslam10:hammadaslam10@task-01.aavgs.mongodb.net/task_05",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("connected"))
+  .catch(() => console.log("error"))
+  .finally(() => console.log("start"));
+
+app.use(express());
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(cookieParser());
+
+app.listen(3002, () => {
+  console.log("backend is serving on port 3002");
+});
+
+
