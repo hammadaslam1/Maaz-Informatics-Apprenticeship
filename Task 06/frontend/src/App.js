7b8e4c96c482ce3271f8c4ea2afb5671e935d1ff
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-09-05 18:06:33 +0500

    renamed task 06 to task pending

--- Task 06/frontend/src/App.js
+++ Task 06/frontend/src/App.js
@@ -1,11 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Navigations from "./router/Navigations";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="App App-header">
-      <Navigations />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
