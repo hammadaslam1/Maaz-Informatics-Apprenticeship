f7d7020d1c0d436d2e2e8e6d529d45fb99ac31d0
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-09-23 00:11:06 +0500

    integrated redux in the frontend

---
+++ Task 08 (chat app)/frontend/src/redux/Store.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { applyMiddleware, combineReducers } from "redux";
+import storage from "redux-persist/lib/storage";
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userReducer/UserReducer";
+import { persistReducer, persistStore } from "redux-persist";
+
+const reducers = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const Store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const Persistor = persistStore(Store);
---
+++ Task 08 (chat app)/frontend/src/redux/reducers/UserReducer.jsx
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signinSuccess: (state, action) => {
+      state.currentUser = action.payload;
+    },
+    signinFailure: (state) => {
+      state.currentUser = null;
+    },
+    signoutSuccess: (state) => {
+      state.currentUser = null;
+    },
+  },
+});
+
+export const { signinSuccess, signinFailure, signoutSuccess } =
+  userSlice.actions;
+
+export default userSlice.reducer;
