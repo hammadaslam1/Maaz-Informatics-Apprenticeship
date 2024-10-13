87dd0428f8c1776e67f84fbcca1eab4f471869b9
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-10-13 23:12:42 +0500

    changed the model names using prisma

---
+++ chatapp_admin_user/prisma/migrations/20241013165848_rename_models/migration.sql
@@ -0,0 +1,59 @@
+/*
+  Warnings:
+
+  - You are about to drop the `conversation` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `message` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `conversation`;
+
+-- DropTable
+DROP TABLE `message`;
+
+-- DropTable
+DROP TABLE `user`;
+
+-- CreateTable
+CREATE TABLE `Users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `is_admin` BOOLEAN NOT NULL DEFAULT false,
+    `full_access` BOOLEAN NOT NULL DEFAULT false,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Users_username_key`(`username`),
+    UNIQUE INDEX `Users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Messages` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `conversation_id` INTEGER NOT NULL,
+    `sender_id` INTEGER NOT NULL,
+    `text` VARCHAR(191) NULL,
+    `type` ENUM('text', 'image', 'application', 'video', 'audio') NOT NULL DEFAULT 'text',
+    `status` ENUM('sent', 'delivered', 'seen') NOT NULL DEFAULT 'sent',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Conversations` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NULL,
+    `admin_id` INTEGER NULL,
+    `last_message` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
