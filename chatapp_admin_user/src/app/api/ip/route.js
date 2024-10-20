e5a1371a4aadc76007349438062e1edd53a21e81
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-10-20 19:30:39 +0500

    added ip route in the app

---
+++ chatapp_admin_user/src/app/api/ip/route.js
@@ -0,0 +1,21 @@
+import os from "os";
+
+export const GET = async (req) => {
+  const networkInterfaces = os.networkInterfaces();
+  let ipAddress = "";
+
+  for (const interfaceName in networkInterfaces) {
+    for (const iface of networkInterfaces[interfaceName]) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        ipAddress = iface.address;
+        break;
+      }
+    }
+    if (ipAddress) break;
+  }
+  return NextResponse.json({
+    success: true,
+    message: "app is running successfully",
+    ip: ipAddress,
+  });
+};
